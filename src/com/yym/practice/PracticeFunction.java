a641662118944af27a409317e9c00dbd1913425d
sx-9665 <sx-yuym@dtdream.com>
2021-02-26 11:23:09 +0800

    0226 添加PracticeFunction

---
+++ src/com/yym/practice/PracticeFunction.java
@@ -0,0 +1,39 @@
+package com.yym.practice;
+
+import org.junit.Test;
+
+/**
+ * 方法练习
+ */
+public class PracticeFunction {
+
+    @Test
+    public void practice1() {
+        int num1 = 10;
+        int num2 = 20;
+        swap(num1, num2);
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    @Test
+    public void practice2() {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(arr[0]);
+        change(arr);
+        System.out.println(arr[0]);
+    }
+
+    public static void change(int[] array) {
+        // 将数组的第⼀个元素变为0
+        array[0] = 0;
+    }
+}
